a672844609346e142e9fbc7ba470413c452149e8
tngus2sh <101691649+tngus2sh@users.noreply.github.com>
2023-02-04 16:34:40 +0900

    [BOJ] 단어 뒤집기 / 브론즈 1 / 10분 / 22.02.04

---
+++ Baekjoon/q9093.java
@@ -0,0 +1,34 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 단어 뒤집기 / 브론즈 1 / 10분
+ * https://www.acmicpc.net/problem/9093
+ */
+public class q9093 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int T = Integer.parseInt(br.readLine());
+        
+        StringTokenizer st ;
+        StringBuilder sb;
+        for(int t = 0; t < T; t++) {
+            st = new StringTokenizer(br.readLine());
+            sb = new StringBuilder();
+            while(st.hasMoreTokens()) {
+                String s = st.nextToken();
+                for (int i = s.length() - 1; i >= 0; i--) {
+                    sb.append(s.charAt(i));
+                }
+                sb.append(" ");
+            }
+            System.out.println(sb);
+        }
+        
+    }
+}

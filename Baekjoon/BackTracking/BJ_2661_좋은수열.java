4021aed57da3f70bc3589a23b988e6beafa19810
tngus2sh <ssuhyeon98@gmail.com>
2023-03-01 21:17:11 +0900

    [BOJ] 좋은수열 / 골드 4 / 2시간 / 23.03.01

--- Baekjoon/BackTracking/BJ_2661_좋은수열.java
+++ Baekjoon/BackTracking/BJ_2661_좋은수열.java
@@ -3,88 +3,55 @@ package BackTracking;
 import java.util.LinkedList;
 import java.util.Scanner;
 
+/**
+ * 좋은수열 / 골드 4 / 2시간
+ * https://www.acmicpc.net/problem/2661
+ */
 public class BJ_2661_좋은수열 {
-	// 부분집합을 위한 선택 체크
-	static boolean[] setCheck;
-	// 부분집합 담아두는 리스트
-	static LinkedList<String> subset;
-	// 조합을 위한 선택 체크
-	static boolean[] combinationCheck;
-	
-	static String str;
-	static int strLen;
+	static int N;
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
 		// 길이 : N
-		int N = sc.nextInt();
-		
-		setCheck = new boolean[3];
-		subset = new LinkedList<>();
-		
-		pickSet(0);
+		N = sc.nextInt();
+
+		// 길이가 1이라면 1이 제일 작은 좋은 수열이므로 1을 출력하고 종료
+		if(N == 1) {
+			System.out.println(1);
+			return;
+		}
+
+		// 주어진 숫자로 수열 만들기
+		makeInput("");
+
 		
 		sc.close();
 	}
-	
-	static void pickSet(int cnt) {
-		if(cnt == 3) {
-			makeSequence();
-			return;
-		}
-		
-		setCheck[cnt] = true;
-		pickSet(cnt + 1);
-		setCheck[cnt] = false;
-		pickSet(cnt + 1);
-	}
-	
-	/** 수열 만들어줌 */
-	static void makeSequence() {
-		StringBuilder sb = new StringBuilder();
-		
-		for (int i = 0; i < 3; i++) {
-			if(setCheck[i]) {
-				sb.append(i);
-			}
-		}
-		
-//		if(sb.toString().length() > 0) {
-//			subset.add(sb.toString());
-//			str = sb.toString();
-//		}
-		str = sb.toString();
-		
-		System.out.println(str);
-		
-		// 만들어진 부분집합들로 조합 만듦
-		combinationCheck = new boolean[subset.size()];
-		strLen = str.length();
-		makeCombination(0);
-		
-	}
-	
-	static void makeCombination(int cnt) {
-		if(cnt == strLen) {
-			check();
-			return;
-		}
-		
-		for (int i = 0; i < strLen; i++) {
-			if(!combinationCheck[i]) {
-				combinationCheck[i] = true;
-				makeCombination(cnt + 1);
+
+	/** 수열 만드는 메소드*/
+	static void makeInput(String result) {
+		if(result.length() == N) {
+			System.out.println(result);
+			System.exit(0);
+		} else {
+			for (int i = 1; i <= 3; i++) {
+				if(twoPointer(result + i)) {
+					makeInput(result + i);
+				}
 			}
 		}
 	}
-	
-	static void check() {
-		for (int i = 0; i < strLen; i++) {
-			if(combinationCheck[i]) {
-				System.out.print(str.charAt(i));
+
+	/**포인터로 좋은수열인지 판별하는 메소드*/
+	static boolean twoPointer(String str) {
+		int length = str.length()/2;
+
+		for (int i = 1; i <= length; i++) {
+			if(str.substring(str.length() - i).equals(str.substring(str.length() - i*2, str.length() - i))) {
+				return false;
 			}
 		}
-		System.out.println();
+		return true;
 	}
 }

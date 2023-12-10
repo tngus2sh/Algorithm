58fabb5b38da5ab5b9949990c3125b5977a18689
tngus2sh <ssuhyeon98@gmail.com>
2023-12-10 16:03:12 +0900

    Feat: 폴리오미노

---
+++ src/Baekjoon/Greedy/BJ_1343_폴리오미노.java
@@ -0,0 +1,51 @@
+package Baekjoon.Greedy;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 문제 이름 / 티어 / 걸린 시간 / 푼 날짜
+ * 폴리오미노 / 실버5 / 21:40 / 231210
+ */
+public class BJ_1343_폴리오미노 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+
+        StringBuilder sb = new StringBuilder();
+
+        int xNum = 0;
+        for (int i =0; i < input.length(); i++) {
+            if (input.charAt(i) == '.') {
+                // 1. 홀수라면 -1 출력
+                if (xNum % 2 != 0) {
+                    System.out.println("-1");
+                    return;
+                }
+                // 2. 4로 나누어 떨어지는 만큼 AAAA로 대치
+                int A = xNum / 4;
+                int B = (xNum % 4)/2;
+                sb.append("AAAA".repeat(Math.max(0, A)));
+                sb.append("BB".repeat(Math.max(0, B)));
+                sb.append(".");
+                // xNum 초기화
+                xNum = 0;
+            } else {
+                xNum++;
+            }
+        }
+
+        // 1. 홀수라면 -1 출력
+        if (xNum % 2 != 0) {
+            System.out.println("-1");
+            return;
+        }
+        // 2. 4로 나누어 떨어지는 만큼 AAAA로 대치
+        int A = xNum / 4;
+        int B = (xNum % 4)/2;
+        sb.append("AAAA".repeat(Math.max(0, A)));
+        sb.append("BB".repeat(Math.max(0, B)));
+
+        System.out.println(sb.toString());
+    }
+}

a567d2311b586f383e5c0aeda4b77a895fcf0126
tngus2sh <ssuhyeon98@gmail.com>
2023-12-10 16:22:03 +0900

    Refactor: 폴리오미노

--- src/Baekjoon/Greedy/BJ_1343_폴리오미노.java
+++ src/Baekjoon/Greedy/BJ_1343_폴리오미노.java
@@ -16,35 +16,30 @@ public class BJ_1343_폴리오미노 {
 
         int xNum = 0;
         for (int i =0; i < input.length(); i++) {
-            if (input.charAt(i) == '.') {
-                // 1. 홀수라면 -1 출력
-                if (xNum % 2 != 0) {
-                    System.out.println("-1");
-                    return;
-                }
-                // 2. 4로 나누어 떨어지는 만큼 AAAA로 대치
-                int A = xNum / 4;
-                int B = (xNum % 4)/2;
-                sb.append("AAAA".repeat(Math.max(0, A)));
-                sb.append("BB".repeat(Math.max(0, B)));
+            if (input.charAt(i) == '.' ) {
                 sb.append(".");
-                // xNum 초기화
-                xNum = 0;
             } else {
                 xNum++;
+                char next = 'X';
+                if (i < input.length()-1) next = input.charAt(i+1);
+                else next = 'N';
+
+                if (next == '.' || next == 'N') {
+                    // 1. 홀수라면 -1 출력
+                    if (xNum % 2 != 0) {
+                        System.out.println("-1");
+                        return;
+                    }
+                    // 2. 4로 나누어 떨어지는 만큼 AAAA로 대치
+                    int A = xNum / 4;
+                    int B = (xNum % 4)/2;
+                    sb.append("AAAA".repeat(A));
+                    sb.append("BB".repeat(B));
+                    // xNum 초기화
+                    xNum = 0;
+                }
             }
         }
-
-        // 1. 홀수라면 -1 출력
-        if (xNum % 2 != 0) {
-            System.out.println("-1");
-            return;
-        }
-        // 2. 4로 나누어 떨어지는 만큼 AAAA로 대치
-        int A = xNum / 4;
-        int B = (xNum % 4)/2;
-        sb.append("AAAA".repeat(Math.max(0, A)));
-        sb.append("BB".repeat(Math.max(0, B)));
 
         System.out.println(sb.toString());
     }

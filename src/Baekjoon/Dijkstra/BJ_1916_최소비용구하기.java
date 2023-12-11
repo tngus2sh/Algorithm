992f00425df74951102f55d3a3c2d4ad3db1cd93
tngus2sh <ssuhyeon98@gmail.com>
2023-12-11 23:12:43 +0900

    Feat: 최소비용 구하기

---
+++ src/Baekjoon/Dijkstra/BJ_1916_최소비용구하기.java
@@ -0,0 +1,76 @@
+package Baekjoon.Dijkstra;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 문제 이름 / 티어 / 걸린 시간 / 푼 날짜
+ * 최소비용구하기 / 골5 / 10:59 / 231211
+ */
+
+public class BJ_1916_최소비용구하기 {
+
+    public static class Node implements Comparable<Node> {
+        int index;
+        int cost;
+        public Node (int index, int cost) {
+            this.index = index;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return Integer.compare(this.cost, o.cost);
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+
+        ArrayList<Node>[] graph = new ArrayList[N+1];
+        for (int i = 0; i < N+1; i++)  graph[i] = new ArrayList<>();
+
+        for (int i =0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+
+            graph[u].add(new Node(v, w));
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+
+        boolean[] check = new boolean[N+1];
+        int[] dist = new int[N+1];
+
+        int INF = Integer.MAX_VALUE;
+        Arrays.fill(dist, INF);
+        dist[start] = 0;
+
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.offer(new Node(start, 0));
+
+        while(!pq.isEmpty()) {
+            int nowVertex = pq.poll().index;
+
+            if (check[nowVertex]) continue;
+            check[nowVertex] = true;
+
+            for (Node next : graph[nowVertex]) {
+                if (dist[next.index] > dist[nowVertex] + next.cost) {
+                    dist[next.index] = dist[nowVertex] + next.cost;
+
+                    pq.offer(new Node(next.index, dist[next.index]));
+                }
+            }
+        }
+
+        System.out.println(dist[end]);
+    }
+}

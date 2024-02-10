b5c4c32adf970bea88f99f5bae94249bb6d27d93
tngus2sh <ssuhyeon98@gmail.com>
2024-02-10 22:07:23 +0900

    Feat: Hashing

--- src/Baekjoon/BJ_15829_Hashing.java
+++ src/Baekjoon/BJ_15829_Hashing.java
@@ -1,7 +1,7 @@
 package Baekjoon;
 
 import java.io.*;
-import java.util.*;
+import java.math.BigInteger;
 
 /**
  * 문제 이름 / 티어 / 걸린 시간 / 푼 날짜
@@ -19,13 +19,13 @@ public class BJ_15829_Hashing {
 
         int r = 31;
         int M = 1234567891;
-        int answer = 0;
+        BigInteger answer = new BigInteger("0");
         for (int i = 0; i < str.length(); i++) {
             int a = str.charAt(i) - 96;
-            answer += a * Math.pow(r, i);
+            answer = answer.add(BigInteger.valueOf(a).multiply(BigInteger.valueOf(r).pow(i)));
         }
 
-        System.out.println(answer%M);
+        System.out.println(answer.mod(BigInteger.valueOf(M)));
 
     }
 }

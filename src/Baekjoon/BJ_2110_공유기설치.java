216c89456aa5a6e8baadd0810eaa9d2f00464d3a
tngus2sh <ssuhyeon98@gmail.com>
2024-02-17 22:50:32 +0900

    Feat: 공유기 설치

---
+++ src/Baekjoon/BJ_2110_공유기설치.java
@@ -0,0 +1,62 @@
+package Baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 문제 이름 / 티어 / 걸린 시간 / 푼 날짜
+ * 공유기설치 / 골드4 / 2시간 / 24.02.17
+ */
+
+public class BJ_2110_공유기설치 {
+
+    static int[] graph;
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        graph = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            graph[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(graph);
+
+        int lo = 1;
+        int hi = graph[n-1] - graph[0] + 1;
+
+        while (lo < hi) {
+            int mid = (hi + lo)/2;
+
+            if (canInstall(mid) < c) {
+                hi = mid;
+            }
+            else {
+                lo = mid + 1;
+            }
+        }
+
+        System.out.println((lo - 1));
+    }
+
+    public static int canInstall(int distance) {
+        int count = 1;
+        int lastLocate = graph[0];
+
+        for (int i = 1; i < graph.length; i++) {
+            int locate = graph[i];
+
+            if (locate - lastLocate >= distance) {
+                count++;
+                lastLocate = locate;
+            }
+        }
+        return count;
+    }
+}
